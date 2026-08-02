ca1f1cf9519df19b0321235316095a622d04482f
wagnerdacor22 <wagnerdacor@gmail.com>
2026-08-02 12:17:06 -0300

    Atualizar o config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,13 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv() # Carrega o arquivo .env
-
+# API Keys
 API_FOOTBALL_KEY = os.getenv("API_FOOTBALL_KEY")
 ODDS_API_KEY = os.getenv("ODDS_API_KEY")
-BANCA_INICIAL = float(os.getenv("BANCA_INICIAL", 1000.0))
+FOOTBALL_DATA_KEY = os.getenv("FOOTBALL_DATA_KEY")
+
+# Telegram
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+# Banca
+BANCA_INICIAL = float(os.getenv("BANCA_INICIAL", "1000"))
